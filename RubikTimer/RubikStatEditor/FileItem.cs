b9d2e3085d89ca04831b7a36c552ef8fb29ac108
krepsy3 <30734190+krepsy3@users.noreply.github.com>
2017-08-05 12:49:00 +0200

    Změna jména složky RUbil :D

---
+++ RubikTimer/RubikStatEditor/FileItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RubikTimer;
+
+namespace RUbil
+{
+    class FileItem
+    {
+        public Statistic Stat { get; private set; }
+        public bool IsStatistic { get; private set; }
+        public string Comment { get; private set; }
+    }
+}
