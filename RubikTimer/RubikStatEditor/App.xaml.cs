6ca505feffb94a3bce50fa25bc2be390cb1719e6
Micovec <30734146+Micovec@users.noreply.github.com>
2017-08-05 15:33:37 +0200

    KURVO

---
+++ RubikTimer/RubikStatEditor/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RubikStatEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string[] args;
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            args = e.Args;
+
+            if (e.Args.Length == 0)
+            {
+                // opening RubikStatEditor.exe not .stxt file
+                MessageBox.Show("To edit statistic files, please open the main app (RubikTimer.exe) and select Edit Statistic Files. Do you wish to launch now?","Fatal error",MessageBoxButton.OKCancel,MessageBoxImage.Error,MessageBoxResult.OK);
+                Shutdown();
+            }
+            else
+            {
+                MainWindow win = new MainWindow();
+                win.Show();
+            }
+        }
+    }
+}
---
+++ RubikTimer/RubikStatEditor/FileReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RubikTimer;
+
+namespace RubikStatEditor
+{
+    class FileReader
+    {
+        public List<FileItem> LoadFileItemsFromFile(string path)
+        {
+            List<FileItem> items = new List<FileItem>();
+
+            // DOPICI, POKUD NEKDO Z NAS COMMITNE SOUBOR, KTERY TEN DRUHY MA TAKY UPRAVENY ALE NE COMMITNUTY TAK TO DELA PICOVINY, TAKZE POKUD NEDELAS NEJAKE VELKE ZMENY A UPRAVUJES JENOM NAZVY PROMENNYCH TAK TO NECH BYT. TED JSEM DELAL CELE DOPOLEDNE NA EDITORU A KVULI TVE UPRAVE TO MUSIM ZNOVA NAPSAT
+
+            return items;
+        }
+    }
+}
