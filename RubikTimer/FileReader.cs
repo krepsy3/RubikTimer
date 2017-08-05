69e429efcc452e96ae56f9775640ddc09f8dab00
krepsy3 <30734190+krepsy3@users.noreply.github.com>
2017-08-05 13:26:20 +0200

    Tak teď už snad jo :D Znovuzaložení projektu a nakopírování tříd a XAMLček

---
+++ RubikTimer/FileReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RubikTimer;
+
+namespace RubikStatEditor
+{
+    class FileReader
+    {
+        public List<FileItem> LoadFileItemsFromFile(string path)
+        {
+            List<FileItem> items = new List<FileItem>();
+
+
+
+            return items;
+        }
+    }
+}
